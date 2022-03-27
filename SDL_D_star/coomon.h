f8be5548a995ab6d6b6960ff84cc8f2995cfc1cf
Blextor <78981647+Blextor@users.noreply.github.com>
2022-03-27 14:18:50 +0200

    Táblázat a futásidőről
    Hiányzik az editor-ból a start és cél mezők megadása.
    Mozgatható falak letétele.
    Falak folyamatos letétele.

--- SDL_D_star/coomon.h
+++ SDL_D_star/coomon.h
@@ -32,9 +32,9 @@
 #define FLUID 4.0f          // folyamatos mozgás során milyen bontást alkalmazzon (lehetőleg egészet)
 #define PGX 15              // pálya szélessége
 #define PGY 15              // és magassága
-#define EXTRA_STEPS 0.3f    // pálya magasságához és szélességéhez képest mennyi extra lépést engedünk meg
+#define EXTRA_STEPS 1.f    // pálya magasságához és szélességéhez képest mennyi extra lépést engedünk meg
 #define PLY 300            // kezdeti véletlen gyalogosok száma
-#define FORGALOM 5
+#define FORGALOM 2
 #define FALCNT 30
 
 #define DEBUG true
